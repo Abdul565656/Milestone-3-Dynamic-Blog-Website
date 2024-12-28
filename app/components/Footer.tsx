f5d31733f5a13c9cc319d433af8169c33117cfeb
abdul565656 <abdullahkashif12b3@gmail.com>
2024-12-28 17:56:45 +0500

    footer update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <div className='bg-orange-500 text-white p-3 text-center'>
+        <div className='bg-orange-500 text-white p-3 text-center font-semibold'>
         <h3>♥Made with ❣ by Abdullah Kashif♥</h3>
         </div>
     </div>
